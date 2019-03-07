c22a769c749b8b9546c46635486a4cbed41cff11
Connor Imes <cimes@isi.edu>
2019-03-07 12:51:24 -0500

    openmp-test, pthreads-test: cleanup sources

--- meta-hpsc-bsp/recipes-tests/openmp-test/files/count_omp_threads.c
+++ meta-hpsc-bsp/recipes-tests/openmp-test/files/count_omp_threads.c
@@ -7,21 +7,20 @@
 
 int main (int argc, char *argv[]) 
 {
-  int nthreads, tid;
+    int nthreads, tid;
 
-  /* Fork a team of threads giving them their own copies of variables */
-#pragma omp parallel private(nthreads, tid)
-  {
-    /* Obtain thread number */
-    tid = omp_get_thread_num();
+    /* Fork a team of threads giving them their own copies of variables */
+    #pragma omp parallel private(nthreads, tid)
+    {
+        /* Obtain thread number */
+        tid = omp_get_thread_num();
 
-    /* Only master thread does this */
-    if (tid == 0) 
-      {
-	nthreads = omp_get_num_threads();
-	printf("Number of threads = %d\n", nthreads);
-      }
+        /* Only master thread does this */
+        if (tid == 0) {
+            nthreads = omp_get_num_threads();
+            printf("Number of threads = %d\n", nthreads);
+        }
+    }  /* All threads join master thread and disband */
 
-  }  /* All threads join master thread and disband */
-
+    return 0;
 }
--- meta-hpsc-bsp/recipes-tests/pthreads-test/files/count_pthreads.c
+++ meta-hpsc-bsp/recipes-tests/pthreads-test/files/count_pthreads.c
@@ -1,30 +1,48 @@
 /******************************************************************************
  * FILE: count_pthreads.c
  ******************************************************************************/
+#include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void *Exit(void *threadid)
 {
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-  int num_threads = atoi(argv[1]);
-  pthread_t threads[num_threads];
-  int rc;
-  long t;
-  for(t=0;t<num_threads;t++){
-    rc = pthread_create(&threads[t], NULL, Exit, (void *)t);
-    if (rc){
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
+    int num_threads;
+    long t;
+    int rc;
+    pthread_t *threads;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s NTHREADS\n", argv[0]);
+        return EINVAL;
     }
-  }
-  printf("Created and destroyed %d threads using pthread_create() and pthread_exit()\n", num_threads);
+    num_threads = atoi(argv[1]);
+    if (num_threads <= 0) {
+        fprintf(stderr, "ERROR: NTHREADS must be > 0\n");
+        return EINVAL;
+    }
+    threads = malloc(num_threads * sizeof(pthread_t));
+    if (!threads) {
+        perror("malloc");
+        return errno;
+    }
+    for (t = 0; t < num_threads; t++) {
+        rc = pthread_create(&threads[t], NULL, Exit, (void *)t);
+        if (rc) {
+            fprintf(stderr, "ERROR: return code from pthread_create() is %d\n",
+                    rc);
+            free(threads);
+            return rc;
+        }
+    }
+    printf("Created and destroyed %d threads using pthread_create() and pthread_exit()\n",
+           num_threads);
 
-  /* Last thing that main() should do */
-  pthread_exit(NULL);
+    free(threads);
+    return 0;
 }
